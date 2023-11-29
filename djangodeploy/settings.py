4a81f14fff704dc22782a611770fbc25ea306d21
Peter Kure <peterkure256@gmail.com>
2023-11-29 17:01:21 +0300

    Feat: Allowed Hosts

--- djangodeploy/settings.py
+++ djangodeploy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8ui&x+fn(=q269t&v^vcoflmqb+o+=k&dntc5nw@50$2$y&=fl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
